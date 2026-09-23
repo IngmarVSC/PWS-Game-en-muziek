fa41e2b4270e61a68c55df5d8749099457844d1d
Ingmar <ingmar@MacBook-Air-van-Ingmar.local>
2026-09-23 14:00:27 +0200

    added basic menu and gameover screens

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,10 +1,42 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject startMenuPanel;
+    public GameObject gameOverPanel;
+
+    void Start()
+    {
+        // Game starts paused until the player presses START
+        Time.timeScale = 0f;
+
+        startMenuPanel.SetActive(true);
+        gameOverPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        startMenuPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
     public void GameOver()
     {
         Debug.Log("GAME OVER");
+
+        gameOverPanel.SetActive(true);
+
         Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().buildIndex
+        );
     }
 }
