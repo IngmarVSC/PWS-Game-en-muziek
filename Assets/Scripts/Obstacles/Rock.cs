0de4aa2f944c602d1d48b1af4d9f699e8ffcbd5e
Ingmar <ingmar@MacBook-Air-van-Ingmar.local>
2026-09-24 10:00:07 +0200

    made asteroids spin around their axis

--- Assets/Scripts/Obstacles/Rock.cs
+++ Assets/Scripts/Obstacles/Rock.cs
@@ -3,24 +3,42 @@ using UnityEngine;
 public class Rock : MonoBehaviour
 {
     public float speed = 10f;
-
     public GameObject[] asteroidModels;
 
-    // random asteroid mesh logic
-    void Start(){
-        int randomIndex = Random.Range(0, asteroidModels.Length);
-        asteroidModels[randomIndex].SetActive(true);
+    private Vector3 randomRotationAxis;
+    private float randomRotationSpeed;
+
+    void Start()
+    {
+        // choose 1 of 5 asteroid models
+        if (asteroidModels.Length > 0)
+        {
+            int randomIndex = Random.Range(0, asteroidModels.Length);
+            asteroidModels[randomIndex].SetActive(true);
+        }
+
+        // generate a random rotation vector
+        randomRotationAxis = new Vector3(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f)
+        ).normalized;
+
+        randomRotationSpeed = Random.Range(40f, 80f); // varies rock spin speed
     }
 
-    // moving rocks towards player
     void Update()
     {
+        // rock moves towards player
         transform.position += Vector3.back * speed * Time.deltaTime;
+
+        // rotate rock around axis
+        transform.Rotate(randomRotationAxis * randomRotationSpeed * Time.deltaTime, Space.Self);
     }
 
-    // destroys rocks when moving over invis wall with destroywall tag
     private void OnTriggerEnter(Collider other)
     {
+        // destroy rock at invis wall
         if (other.gameObject.CompareTag("Trigger_DestroyWall"))
         {
             Destroy(gameObject);
